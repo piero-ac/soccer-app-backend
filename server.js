43468efe74289c412a785253fa0d8c1a4fa727d1
Piero Coronado <pacoronado01@gmail.com>
2023-07-06 15:35:07 -0400

    Create backend endpoint urls

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get("/soccer/helloworld", (req, res) => {
 	res.json("Hello World!");
 });
 
+app.get("/soccer/table/:league/:season", async (req, res) => {});
+
+app.get("/soccer/topscorers/:league/:season", async (req, res) => {});
+
+app.get("/soccer/matches/:league/:season", async (req, res) => {});
+
+app.get("/soccer/match/:id/events", async (req, res) => {});
+
+app.get("/soccer/match/:id/stats", async (req, res) => {});
+
+app.get("/soccer/match/:id/lineups", async (req, res) => {});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
