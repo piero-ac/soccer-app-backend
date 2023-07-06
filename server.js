b3a623c9a00acf854febee822ccc1a814301c546
Piero Coronado <pacoronado01@gmail.com>
2023-07-06 16:06:29 -0400

    Add functionality for fetching and parsing standings data

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
+const axios = require("axios");
+import parseStandings from "./parsing-functions/parseStandings";
 
 app.get("/soccer/helloworld", (req, res) => {
-	res.json("Hello World!");
+	res.json(`Hello World!`);
 });
 
-app.get("/soccer/table/:league/:season", async (req, res) => {});
+app.get("/soccer/table/:league/:season", async (req, res) => {
+	console.log("request to backend");
+	const { league, season } = req.params;
+	const options = {
+		method: "GET",
+		url: "https://api-football-v1.p.rapidapi.com/v3/standings",
+		params: {
+			season: season,
+			league: league,
+		},
+		headers: {
+			"X-RapidAPI-Key": process.env.RAPID_API_KEY,
+			"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+		},
+	};
+
+	try {
+		const response = await axios.request(options);
+		const statusCode = response.status;
+		if (statusCode === 200) {
+			const data = response.data.response[0].league.standings[0];
+			const parsedData = parseStandings(data);
+			return res.json({ data: parsedData });
+		} else {
+			return res.json({
+				error: "Could not obtain standings from API-Football",
+			});
+		}
+	} catch (error) {
+		return res.json({ error });
+	}
+});
 
 app.get("/soccer/topscorers/:league/:season", async (req, res) => {});
 
