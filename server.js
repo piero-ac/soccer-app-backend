11f89d345dba0e68bb91975786deee4714b2dd56
Piero Coronado <pacoronado01@gmail.com>
2023-07-06 16:37:30 -0400

    Add functionality for fetching top scorers data

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const axios = require("axios");
-import parseStandings from "./parsing-functions/parseStandings";
+const parseStandings = require("./parsing-functions/parseStandings.js");
+const parseTopScorers = require("./parsing-functions/parseTopScorers.js");
 
 app.get("/soccer/helloworld", (req, res) => {
 	res.json(`Hello World!`);
 });
 
 app.get("/soccer/table/:league/:season", async (req, res) => {
-	console.log("request to backend");
+	console.log("request to backend for table");
+
 	const { league, season } = req.params;
 	const options = {
 		method: "GET",
@@ -42,7 +44,39 @@ app.get("/soccer/table/:league/:season", async (req, res) => {
 	}
 });
 
-app.get("/soccer/topscorers/:league/:season", async (req, res) => {});
+app.get("/soccer/topscorers/:league/:season", async (req, res) => {
+	console.log("request to backend for topscorers");
+
+	const { league, season } = req.params;
+	const options = {
+		method: "GET",
+		url: "https://api-football-v1.p.rapidapi.com/v3/players/topscorers",
+		params: {
+			league: league,
+			season: season,
+		},
+		headers: {
+			"X-RapidAPI-Key": process.env.RAPID_API_KEY,
+			"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+		},
+	};
+
+	try {
+		const response = await axios.request(options);
+		const statusCode = response.status;
+		if (statusCode === 200) {
+			const data = response.data.response;
+			const parsedData = parseTopScorers(data);
+			return res.json({ data: parsedData });
+		} else {
+			return res.json({
+				error: "Could not obtain top scorers from API-Football",
+			});
+		}
+	} catch (error) {
+		return res.json({ error });
+	}
+});
 
 app.get("/soccer/matches/:league/:season", async (req, res) => {});
 
