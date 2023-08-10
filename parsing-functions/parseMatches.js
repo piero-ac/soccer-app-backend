59ec209962f504187538dcf25e83ee351fe6e7f6
Piero Coronado <pacoronado01@gmail.com>
2023-08-10 10:14:09 -0400

    Add new time related fields for parsing

--- parsing-functions/parseMatches.js
+++ parsing-functions/parseMatches.js
@@ -11,6 +11,11 @@ function parseMatches(data) {
 				month: "long",
 				day: "numeric",
 			}),
+			time: getTimeInEST(match.fixture.timestamp),
+			dateISOFormat: {
+				timezone: match.fixture.timezone,
+				ISOString: match.fixture.date,
+			},
 			id: match.fixture.id,
 			homeTeam: { logo: match.teams.home.logo, name: match.teams.home.name },
 			awayTeam: { logo: match.teams.away.logo, name: match.teams.away.name },
@@ -22,6 +27,24 @@ function parseMatches(data) {
 	}
 
 	return { parsedData, rounds: [...rounds] };
+}
+
+function getTimeInEST(timestamp) {
+	const utcDate = new Date(timestamp * 1000);
+
+	// Convert UTC time to Eastern Standard Time (EST)
+	const estOffset = -5 * 60 * 60 * 1000; // EST offset is -5 hours
+	const estDate = new Date(utcDate.getTime() + estOffset);
+
+	// Get the hours and minutes in EST
+	const estHours = estDate.getUTCHours();
+	const estMinutes = estDate.getUTCMinutes();
+
+	// Format the hours and minutes as HH:MM
+	const estTime = `${estHours.toString().padStart(2, "0")}:${estMinutes
+		.toString()
+		.padStart(2, "0")}`;
+	return estTime;
 }
 
 module.exports = parseMatches;
